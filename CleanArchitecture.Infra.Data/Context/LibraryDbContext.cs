ccd95c4840a1005b19c86a12fd05f8300dda9f78
Lord Suru <fabrosell@gmail.com>
2024-07-26 20:57:37 -0400

    First push, MVC project with program.cs wired to the code first user and domain models

---
+++ CleanArchitecture.Infra.Data/Context/LibraryDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchitecture.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infra.Data.Context
+{
+    public class LibraryDbContext: DbContext
+    {
+        public LibraryDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+
+    }
+}
